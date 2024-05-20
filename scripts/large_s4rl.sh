a2b063ec493d3777ec76f36e3c0d20a239262b43
Jaewoopudding <ijnn258@gmail.com>
2024-05-20 22:56:00 +0900

    update scripts

--- scripts/large_s4rl.sh
+++ scripts/large_s4rl.sh
@@ -6,10 +6,11 @@ datapath=False
 device=cuda:0
 # S4RL, synther
 
-GDA=None 
+
+GDA=S4RL
 for seed in 0 1 2 3
 do
-    python algorithms/offline/iql.py --env=$env --config_path=$config_path --device=$device --GDA=$GDA --seed=$seed --eval_freq=50000 &
+    python algorithms/offline/iql.py --env=$env --config_path=$config_path--device=$device --GDA=$GDA --seed=$seed --eval_freq=50000 &
 done
 wait
 for seed in 4 5 6 7
